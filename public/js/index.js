f66e87bce047f717035cbc03ee87eb84655ee2b4
Luke Matusiak <lukematusiak@pm.me>
2021-05-29 17:33:49 -0700

    removed join party button and added end party button

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,20 @@
 const navbar = document.querySelector('#top-bar');
 const loginRow = document.querySelector('#loginRow');
 const startBtn = document.querySelector('#start-btn')
+const btnContainer = document.querySelector('#button-container')
+var isMobile = window.matchMedia('(max-width: 576px)');
+
 addEventListeners();
 function addEventListeners(){
-  window.addEventListener('load', setLength);
-  window.addEventListener('resize', setLength);
+  window.addEventListener('load', setSize);
+  window.addEventListener('resize', setSize);
 }
 
-function setLength(e){
+function setSize(e){
   var navHeight = navbar.getBoundingClientRect().height;
   var winHeight = window.innerHeight;
   loginRow.setAttribute("style", `height: ${winHeight - navHeight}`);
+  if(isMobile.matches){
+    btnContainer.classList = 'container rounded-3'
+  }
 }
